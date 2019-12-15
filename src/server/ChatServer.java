f2b8d5bb27f7ae6151663caf9e10991244755e67
Carlos <302642469@qq.com>
2019-12-16 00:10:31 +0800

    添加服务类
    有个问题：老师要求必须用到多线程，那么实现群发消息的实现再用轮询肯定不合适了，不清楚Java里面是怎么传递消息的，好像是事件，明天确认一下。

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -4,5 +4,9 @@ import java.util.Map;
 
 public class ChatServer {
 	Map<String, User> userMap;//储存所有注册成功的用户的个人信息
+	int userNumCeiling;//用户人数上限
+	
+	String hostStr;//本机地址
+	int port;//服务端口
 	
 }
---
+++ src/server/ServerThread.java
@@ -0,0 +1,51 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+/*
+ * 一个线程服务一个用户
+ * */
+public class ServerThread extends Thread {
+	String hostStr;//本机地址
+	int port;//服务端口
+	
+	Socket socket;//服务端socket
+	BufferedReader in;//缓存流
+	PrintStream out;//输出
+	
+	public ServerThread(Socket _socket) throws IOException {
+		socket=_socket;
+		hostStr=socket.getInetAddress().getHostAddress();//local address
+		port=socket.getLocalPort();//local port
+		in=new BufferedReader(new InputStreamReader(socket.getInputStream()));//input
+		out=new PrintStream(socket.getOutputStream());//output
+	}
+	
+	public void run(){
+		systemMsg("Hello,");
+		systemMsg("You connected to the server successfully.");
+		systemMsg("You can talk with others now.");
+		out.close();
+		
+		//结束通讯
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//系统消息
+	public void systemMsg(String msg){
+		msg="[System message]"+msg;
+		out.println(msg);
+	}
+	
+	public void privateMsg(String id,String name){
+		//TODO 私聊功能
+	}
+}
